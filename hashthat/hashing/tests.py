00497649773fe6fee06afab3094e14cbcaa975f3
Tristan Izlar <59484088+tizlar@users.noreply.github.com>
2022-02-18 12:43:18 -0500

    unit test - testing if hashlib function works

--- hashthat/hashing/tests.py
+++ hashthat/hashing/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from selenium import webdriver
 from .forms import HashForm
+import hashlib
 
 # class FunctionalTestCase(TestCase):
 
@@ -30,3 +31,7 @@ class UnitTestCase(TestCase):
     def test_hash_form(self):
         form = HashForm(data={'text':'hello'})
         self.assertTrue(form.is_valid())
+
+    def test_hash_func_works(self):
+        text_hash = hashlib.sha256('hello'.encode('utf-8')).hexdigest()
+        self.assertEqual('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', text_hash)

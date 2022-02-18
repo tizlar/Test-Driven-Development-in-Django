a72ea8633ff824650c840fe19149a33e4bd51609
Tristan Izlar <59484088+tizlar@users.noreply.github.com>
2022-02-18 13:05:36 -0500

    unit test - testing a POST request

--- hashthat/hashing/tests.py
+++ hashthat/hashing/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from selenium import webdriver
 from .forms import HashForm
 import hashlib
+from .models import Hash
 
 # class FunctionalTestCase(TestCase):
 
@@ -35,3 +36,20 @@ class UnitTestCase(TestCase):
     def test_hash_func_works(self):
         text_hash = hashlib.sha256('hello'.encode('utf-8')).hexdigest()
         self.assertEqual('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', text_hash)
+
+    def saveHash(self):
+        hash = Hash()
+        hash.text = 'hello'
+        hash.hash = '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824'
+        hash.save()
+        return hash
+
+    def test_hash_object(self):
+        hash = self.saveHash
+        pulled_hash = Hash.objects.get(hash='2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824')
+        self.assertEqual(hash.text, pulled_hash.text)
+
+    def test_viewing_hash(self):
+        hash = self.saveHash
+        response = self.client.get('/hash/2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824')
+        self.assertContains(response,'hello')
